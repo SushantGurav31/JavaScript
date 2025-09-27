2a32b0fa8a379d39d54578cace97b8348735cbfa
Sushant Gurav <sushantguravfsd@gmail.com>
2025-09-27 22:49:15 +0530

    Add files via upload

---
+++ Creating an Array.js
@@ -0,0 +1,12 @@
+const fruits = ["apple", "banana", "mango"];
+// fruits.push("Orange");
+// fruits.unshift("cherry");
+// fruits[1] = "grapes"
+// fruits.pop("Orange");
+// fruits.shift()
+// delete fruits[1];
+console.log(fruits);
+
+const numbers = [1,2,3,4];
+// numbers.push(22);
+console.log(numbers);
