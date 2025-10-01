693474b4cb061edb5699f696caf23429430f6956
Sushant Gurav <sushantguravfsd@gmail.com>
2025-10-01 07:42:44 +0530

    Add files via upload

---
+++ Function Parameters & Arguments/Default Parameters.js
@@ -0,0 +1,14 @@
+function greet (name = "Guest"){
+    return `Hello, ${name}`;
+}
+
+console.log(greet());  // Hello, Guest 
+
+
+
+function greet1 (name= "Sushant") {
+    return `Hello, ${name}`;
+
+}
+
+console.log(greet1());   // Hello, Sushant
---
+++ Function Parameters & Arguments/Rest Parameters.js
@@ -0,0 +1,8 @@
+// Collect multiple argument into an array.
+function sum (...numbers) {
+    return numbers.reduce((a,b) => a + b , 0);
+}
+
+console.log(sum(1,2,3,4));   //10
+
+
